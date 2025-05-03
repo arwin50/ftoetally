129eab1acc75d447dc874f4da7b432db0584fad7
powtzulist <delasanarwin@gmail.com>
2025-05-03 19:06:35 +0800

    fixed filters in transaction table

--- app/components/transactions/transactionFilters.tsx
+++ app/components/transactions/transactionFilters.tsx
@@ -47,17 +47,18 @@ export default function TransactionFilters({
         </label>
         <div className="relative w-full sm:w-auto">
           <select
-            id="month"
-            value={month}
-            onChange={(e) => setMonth(e.target.value)}
-            className="appearance-none h-10 border border-gray-300 rounded-lg ps-4 pe-7 py-1.5 text-sm sm:text-md text-black w-full sm:w-auto"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="appearance-none h-10 border border-gray-300 rounded-lg ps-4 pe-7 py-1.5 text-sm sm:text-md text-black w-full"
           >
             <option value="All">All</option>
-            {availableMonths.map((monthStr) => (
-              <option key={monthStr} value={monthStr}>
-                {monthStr}
-              </option>
-            ))}
+            <option value="Food">Food</option>
+            <option value="Transportation">Transportation</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Salary">Salary</option>
+            <option value="Other">Other</option>
           </select>
           <div className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-600 text-xs">
             ▼
